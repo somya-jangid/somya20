cf07e0bc8a19dd4512ec63e59f00ded022bd9fa8
somya-jangid <somyajangid78@gmail.com>
2025-02-24 01:11:52 +0530

    Add or update backend/server.js

---
+++ backend/server.js
@@ -0,0 +1,1 @@
+const express = require('express');const mongoose = require('mongoose');const app = express();const PORT = process.env.PORT || 5000;app.use(express.json());app.get('/', (req, res) => {res.send('Netflix Clone API');});app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
